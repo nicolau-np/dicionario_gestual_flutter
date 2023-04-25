a5fa3a122c64efe6632cf3ecfef68b6709503cba
nicolau-np <mr1Normaliii@gmail.com>
2023-04-25 21:28:07 +0100

    update

---
+++ lib/view/game/game1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class GameOne extends StatefulWidget {
+  const GameOne({super.key});
+
+  @override
+  State<GameOne> createState() => _GameOneState();
+}
+
+class _GameOneState extends State<GameOne> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
