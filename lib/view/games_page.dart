e63f123d3afc93a57be3cf7915d2a599f047af19
nicolau-np <mr1Normaliii@gmail.com>
2023-06-06 11:18:31 +0100

    update

--- lib/view/games_page.dart
+++ lib/view/games_page.dart
@@ -24,8 +24,8 @@ class GamePage extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.network(
-                    'https://scontent.flad4-1.fna.fbcdn.net/v/t39.30808-6/343403445_3419019341676270_2471787823684247912_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEtFV27IevlvRfIZJ9rr3bf93YWlW3n6pf3dhaVbefql8nen5pzu_2aYGrB54XI5NLaoAmlnzAYJCdwVA8ogHGd&_nc_ohc=kzOYJdW3eAoAX-XbjSt&_nc_zt=23&_nc_ht=scontent.flad4-1.fna&oh=00_AfDgTKmLsbvtXoCy2wS1OwTRaJixk9Pma1HR8G2RgjKKNg&oe=6452D657',
+                  Image.asset(
+                    'assets/images/jogo.jpeg',
                     height: 100,
                     width: 100,
                   ),
